cbbcc0760dc880c753e818ca2fcba51872e303bc
Jeremy Marquis <jeremy@jeremymarquis.com>
2020-01-20 16:22:50 -0500

    autolearning

---
+++ entity.rb
@@ -0,0 +1,95 @@
+require 'open-uri'
+require 'nokogiri'
+require 'json'
+require 'uri'
+require 'net/https'
+
+class MeatballEntity
+
+  BING_KEY = 'aaf10f9661404d7a88e6ae5af6acdea7'
+  BING_URI = 'https://api.cognitive.microsoft.com'
+  BING_PATH = '/bing/v7.0/search'
+
+  URL_BLACKLIST = [
+    'youtube.com',
+    'youtu.be',
+    'quizlet.com'
+  ]
+
+  # Returns true if successful, false if not
+  def self.learn term = nil
+    tries = 0
+    while tries < 10
+      tries += 1
+
+      begin
+        final_term = term.nil? ? generate(120) : term
+        results = search final_term
+        result_key, result_url = choose_result results
+        doc = fetch result_url
+        text = scrape doc
+        save result_key, text
+      rescue => e
+        p "Oh no! #{e.message}"
+        next
+      end
+
+      p 'Success!'
+      return true
+    end
+
+    return false
+  end
+
+  def self.generate len
+    output = `python3 meatball.py #{len}`.strip
+    raise 'Failed to generate a decent string.' if output.empty? || output == 'None'
+    p "GENERATED: #{output}"
+    output
+  end
+
+  def self.search term
+    p "Searching for '#{term}' ..."
+
+    request_url = URI(BING_URI + BING_PATH + "?q=" + URI.encode_www_form_component(term))
+    request = Net::HTTP::Get.new(request_url)
+    request['Ocp-Apim-Subscription-Key'] = BING_KEY
+
+    response = Net::HTTP.start(request_url.host, request_url.port, use_ssl: request_url.scheme == 'https') do |http|
+      http.request(request)
+    end
+
+    JSON(response.body)['webPages']['value']
+  end
+
+  def self.choose_result results
+    result_url = results.sample['url']
+    result_key = result_url.gsub(/[^0-9A-Za-z]/, '')
+
+    raise "Blacklisted! #{result_url}" if URL_BLACKLIST.any? { |pattern| result_url.include?(pattern) }
+    raise "Already parsed! #{result_url}" if File::exist? "./sources/#{result_key}"
+
+    return result_key, result_url
+  end
+
+  def self.fetch url
+    p "Fetching #{url} ..."
+    Nokogiri::HTML(OpenURI.open_uri(url))
+  end
+
+  def self.scrape doc
+    text = ''
+    doc.css('body').css('p, h1, h2, h3, h4, h5, h6').each do |txt|
+      txtstring = txt.text
+      txtstring.strip!
+      period = ['.', ',', ';', '!', '?'].include?(txtstring[-1]) ? '' : '.'
+      text += txtstring + period + "\n"
+    end
+    text
+  end
+
+  def self.save key, text
+    File.write("./sources/#{key}.txt", text)
+  end
+
+end
--- server.rb
+++ server.rb
@@ -5,131 +5,24 @@ require 'nokogiri'
 require 'json'
 require 'uri'
 require 'net/https'
+require_relative 'entity'
 
 set :bind, '0.0.0.0'
 
 get '/' do
-  @text = `python3 meatball.py 280`.strip
-  p @text
-  return redirect '/learn' if @text.empty?
-  erb :index
-end
 
-get '/test' do
-  doc = Nokogiri::HTML(OpenURI.open_uri(params[:q]))
-
-  text = ''
-  doc.css('body').css('p, h1, h2, h3, h4, h5, h6').each do |txt|
-    txtstring = txt.text
-    txtstring.strip!
-    period = txtstring[-1] == '.' ? '' : '.'
-    text += txtstring + period + "<br>"
-  end
-  text
-end
-
-get '/learn' do
-
-  @term = params[:term]
-  if !Dir::exist? './sources'
-    if @term.nil? || @term.empty?
-      return erb :initial
-    end
-
-    Dir::mkdir './sources'
-  end
-
-  key = 'aaf10f9661404d7a88e6ae5af6acdea7'
-  uri = 'https://api.cognitive.microsoft.com'
-  path = '/bing/v7.0/search'
-
-  blacklist = [
-    'youtube.com',
-    'youtu.be',
-    'quizlet.com'
-  ]
-
-  tries = 0
-
-  while true
-
-    tries += 1
-    if tries > 10
+  if !params[:term].nil?
+    if MeatballEntity.learn params[:term]
+      return redirect '/'
+    else
       return erb :fail
     end
-
-    @term = params[:term] || ''
-
-    # get a search string
-    if @term.nil? || @term.empty?
-      @term = `python3 meatball.py 60`
-      @term.strip!
-
-      next if @term == 'None'
-      return erb :initial if @term == ''
-    end
-
-    p "Searching for '#{@term}' ..."
-
-    # get search results
-    uri = URI(uri + path + "?q=" + URI.encode_www_form_component(@term))
-    request = Net::HTTP::Get.new(uri)
-    request['Ocp-Apim-Subscription-Key'] = key
-    response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
-      http.request(request)
-    end
-
-    # pick a random one
-    begin
-      results = JSON(response.body)['webPages']['value']
-      random_result = results.sample
-      @result_url = random_result['url']
-    rescue => e
-      p e.message
-      p JSON(response.body)
-      next
-    end
-
-    result_key = @result_url.gsub(/[^0-9A-Za-z]/, '')
-
-    if blacklist.any? { |pattern| @result_url.include?(pattern) }
-      p "Blacklisted! #{@result_url}"
-      next
-    end
-
-    if File::exist? "./sources/#{result_key}"
-      p "Already parsed! #{@result_url}"
-      next
-    end
-
-    begin
-      # fetch and parse
-      p "Fetching #{@result_url} ..."
-      doc = Nokogiri::HTML(OpenURI.open_uri(@result_url))
-    rescue e
-      p e.message
-      next
-    end
-
-    break
-
   end
 
-  text = ''
-  doc.css('body').css('p, h1, h2, h3, h4, h5, h6').each do |txt|
-    txtstring = txt.text
-    txtstring.strip!
-    period = txtstring[-1] == '.' ? '' : '.'
-    text += txtstring + period + "\n"
-  end
+  @text = MeatballEntity.generate 280
 
-  # write it to the sources
-  File.write("./sources/#{result_key}.txt", text)
+  return erb :initial if @text.empty?
 
-  if params[:term]
-    return redirect '/learn'
-  end
-
-  erb :learn
+  erb :index
 
 end
