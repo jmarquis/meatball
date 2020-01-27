b4eb5476a5a0aeff0f1d657e830443d2b69d4e3f
Jeremy Marquis <jeremy@jeremymarquis.com>
2020-01-26 19:48:55 -0500

    learn and tweet way less

--- tweet.rb
+++ tweet.rb
@@ -1,16 +1,22 @@
 require 'twitter'
 require_relative 'entity'
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+if rand.round(2) == 1
+
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+  end
+
+  tweet = MeatballEntity.generate 280
+  p "TWEETING: #{tweet}"
+
+  client.update tweet
+
+  p 'Tweet successful!'
+
+else
+  p 'No tweet this time around.'
 end
-
-tweet = MeatballEntity.generate 280
-p "TWEETING: #{tweet}"
-
-client.update tweet
-
-p 'Tweet successful!'
