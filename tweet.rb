149da84a16dc4a62633125958844006519908243
Jeremy Marquis <jeremy@jeremymarquis.com>
2020-05-24 21:28:17 -0400

    better short circuit

--- tweet.rb
+++ tweet.rb
@@ -1,20 +1,18 @@
+exit unless rand.round(3) == 1
+
 require 'twitter'
 require_relative 'entity'
 
-if rand.round(3) == 1
-
-  client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
-    config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
-    config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
-    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
-  end
-
-  tweet = MeatballEntity.generate 280
-  p "TWEETING: #{tweet}"
-
-  client.update tweet
-
-  p 'Tweet successful!'
-
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+  config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+  config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+  config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
 end
+
+tweet = MeatballEntity.generate 280
+p "TWEETING: #{tweet}"
+
+client.update tweet
+
+p 'Tweet successful!'
